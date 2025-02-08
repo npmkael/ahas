6593161167349763911792f953245fbbb2baca84
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-08 22:13:30 +0800

    part 2: leap year exercise

---
+++ day-3/leap-year.py
@@ -0,0 +1,16 @@
+####################################################
+# Part 2 - Programming exercise: Leap year
+####################################################
+
+year = int(input("Please type in a year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("That year is a leap year")
+        else:
+            print("That year is not a leap year")
+    else:
+        print("That year is a leap year.")
+else:
+    print("That year is not a leap year.")
