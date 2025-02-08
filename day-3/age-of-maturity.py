973d40b13b81078c8d40dc6a8c566019578605f5
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-08 10:32:48 +0800

    part 2: Age of maturity exercise

---
+++ day-3/age-of-maturity.py
@@ -0,0 +1,10 @@
+####################################################
+# Part 2 - Programming exercise: Age of maturity
+####################################################
+
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are of age!")
+else:
+    print("You are not of age!")
