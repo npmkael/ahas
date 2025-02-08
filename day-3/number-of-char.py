fdf3570755bb65b23711a15fb1717f32f424f917
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-08 09:41:30 +0800

    part 2: number of characters exercise

---
+++ day-3/number-of-char.py
@@ -0,0 +1,10 @@
+####################################################
+# Part 2 - Programming exercise: Number of characters
+####################################################
+
+word = input("Please type in a word: ")
+
+if len(word) > 2:
+    print(f"There are {len(word)} letters in the word {word}")
+
+print("Thank you!")
