a6016c67730bce70371d89ef0b4e635e58667dca
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-10 06:46:18 +0800

    part 2: repeat password exercise

---
+++ part-2/repeat-password.py
@@ -0,0 +1,14 @@
+####################################################
+# Part 2 - Repeat password exercise
+####################################################
+
+password = input("Password:")
+
+while True: 
+    repeat_password = input("Repeat password: ")
+
+    if repeat_password == password:
+        print("User account created!")
+        break
+    else:
+        print("They do not match!")
