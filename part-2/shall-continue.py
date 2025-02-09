877137407ce3021e96887bbabcd5be54a46e6b54
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-09 20:40:20 +0800

    part 2: shall we continue exercise

---
+++ part-2/shall-continue.py
@@ -0,0 +1,12 @@
+####################################################
+# Part 2 - Programming exercise: Shall we continue?
+####################################################
+
+while True:
+  print("hi")
+  ask = input("Shall we continue? ")
+
+  if ask == "no":
+    print("okay then")
+    break
+ 
