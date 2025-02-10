86a6b4a3fb0a9e61056b9398c8989bbc9de7587b
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-10 20:11:11 +0800

    part 3: string multiplied exercise

---
+++ part-3/string-multi.py
@@ -0,0 +1,8 @@
+####################################################
+# Part 3 - Programming exercise: String multiplied
+####################################################
+
+word = input("Please type in a string: ")
+times = int(input("Please type in a amount: "))
+
+print(word * times)
