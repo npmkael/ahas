fb14af7c32594c84b941aa307cdd0ece4c60b60a
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-10 20:46:21 +0800

    part 3: underlining exercise

---
+++ part-3/underlining.py
@@ -0,0 +1,12 @@
+####################################################
+# Part 3 - Programming exercise: A rectangle of hashes
+####################################################
+
+while sentence:
+  sentence = input("Please type in a string: ")
+
+  if not sentence:
+    break
+
+  print(sentence)
+  print("-" * len(sentence))
