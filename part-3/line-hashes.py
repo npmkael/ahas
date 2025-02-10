152a29ee392cbfe306183baf4caef33b6c396b29
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-10 20:35:57 +0800

    part 3: line of hashes exercise

---
+++ part-3/line-hashes.py
@@ -0,0 +1,6 @@
+####################################################
+# Part 3 - Programming exercise: A line of hashes
+####################################################
+
+width = int(input("Width:"))
+print("#" * width)
