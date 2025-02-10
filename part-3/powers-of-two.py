5d1bb45a263af62b8a60aa28381a780b0f6b45d2
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-10 18:05:46 +0800

    part 3: powers of two exercise

---
+++ part-3/powers-of-two.py
@@ -0,0 +1,10 @@
+####################################################
+# Part 3 - Programming exercise: Powers of two
+####################################################
+
+limit = int(input("Upper limit: "))
+power = 1
+
+while limit >= power:
+  print(power)
+  power *= 2
