cf7decd648250c6e762aec455365119b20ab3fb1
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-10 18:05:28 +0800

    part 3: numbers exercise

---
+++ part-3/numbers.py
@@ -0,0 +1,10 @@
+####################################################
+# Part 3 - Programming exercise: Numbers
+####################################################
+
+upper_limit = int(input("Upper limit: "))
+number = 0
+
+while upper_limit > number + 1:
+  number += 1
+  print(number)
